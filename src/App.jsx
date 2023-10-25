6c62572216776245b94ba75cdcf8705148ba32a3
Raul Low <raulou92@gmail.com>
2023-10-25 16:39:54 -0300

    adding style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { nanoid } from "nanoid";
 import initialItems from "./items";
 import "./App.css";
+import { FaSearch, FaListUl } from "react-icons/fa";
+
+
 
 function App() {
     const [items, setItems] = useState(initialItems);
@@ -28,45 +31,71 @@ function App() {
     const onComplete = (id) => {
         setItems(
             items.map((item) => {
-              return item.id === id ? {...item, completed: !item.completed} : item}));
-    }
-
-    const searcher = (items) => {
-        return items.filter((item) => item.name.toLowerCase().includes(search.trim()));
+                return item.id === id
+                    ? { ...item, completed: !item.completed }
+                    : item;
+            })
+        );
     };
 
-    const liClasses = ""
+    const searcher = (items) => {
+        return items.filter((item) =>
+            item.name.toLowerCase().includes(search.trim())
+        );
+    };
+
+    const liClasses = "";
 
     return (
-        <div className="max-w-2xl w-2/3 border-slate-900 border">
-            <input
-                type="text"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                className="border-slate-900 border"
-            />
-            <ul>
-                {searcher(items).map((item) => (
-                    <li 
-                      key={item.id} 
-                      className={`${liClasses} ${item.completed && "line-through"}`}
-                      onClick={() => onComplete(item.id)}
-                      >
-                        {item.name}
-                        <button onClick={() => onDelete(item.id)}>X</button>
-                    </li>
-                ))}
-            </ul>
-            <form onSubmit={onSubmit}>
-                <input
-                    type="text"
-                    value={newItem}
-                    onChange={(e) => setNewItem(e.target.value)}
-                    className=""
-                />
-                <button type="submit">Add</button>
-            </form>
-        </div>
+        <section className="max-w-2xl w-2/3 h-screen py-24">
+            <div className="bg-neutral-50 p-8 rounded-xl shadow-lg border h-full flex flex-col justify-between">
+                <div>
+                    <div className="flex items-center w-full">
+                        <FaSearch className="absolute pointer-events-none ml-5 text-neutral-500"/>
+                        <input
+                            type="text"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            placeholder="Search"
+                            className="bg-neutral-50 border-neutral-300 border rounded-lg py-3 px-12 w-full"
+                        />
+
+                    </div>
+                    <div className="overflow-hidden">
+                        <ul className="">
+                            {searcher(items).map((item, index) => (
+                                <li key={item.id} className="flex">
+                                    <p>{index + 1}</p>
+                                    <p
+                                        onClick={() => onComplete(item.id)}
+                                        className={`${liClasses} ${
+                                            item.completed && "line-through"
+                                        }`}
+                                    >
+                                        {item.name}
+                                    </p>
+                                    <button onClick={() => onDelete(item.id)}>
+                                        X
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <form onSubmit={onSubmit} className="flex items-center w-full">
+                    <FaListUl className="absolute pointer-events-none ml-5 text-neutral-500"/>
+                    <input
+                        type="text"
+                        value={newItem}
+                        onChange={(e) => setNewItem(e.target.value)}
+                        placeholder="Add new item"
+                        className="bg-neutral-50 border-neutral-300 border rounded-lg py-3 px-12 w-full mr-2"
+                        autoFocus
+                    />
+                    <button className="bg-blue-500 text-neutral-50 rounded-lg py-3 px-5" type="submit">Add</button>
+                </form>
+            </div>
+        </section>
     );
 }
 
