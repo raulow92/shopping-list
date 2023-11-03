27d39c026699c7717691a01c8ada5d3cc4249f82
Raul Low <raulou92@gmail.com>
2023-11-03 13:40:15 -0300

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { nanoid } from "nanoid";
 import initialItems from "./items";
 import "./App.css";
 import { FaSearch, FaListUl } from "react-icons/fa";
-import { FaCartShopping } from "react-icons/fa6";
-import { BiTrash } from "react-icons/bi";
+
+
 
 function App() {
     const [items, setItems] = useState(initialItems);
@@ -13,7 +13,6 @@ function App() {
 
     const onSubmit = (e) => {
         e.preventDefault();
-        if (newItem === "") return;
         setItems([
             ...items,
             {
@@ -26,8 +25,7 @@ function App() {
     };
 
     const onDelete = (id) => {
-        confirm("Are you sure you want to delete this item?") &&
-            setItems(items.filter((item) => item.id !== id));
+        setItems(items.filter((item) => item.id !== id));
     };
 
     const onComplete = (id) => {
@@ -46,78 +44,55 @@ function App() {
         );
     };
 
-    const itemClasses =
-        "font-medium cursor-pointer truncate overflow-hidden w-[420px]";
+    const liClasses = "";
 
     return (
-        <section className="w-[600px] h-screen flex justify-center items-center">
-            <div className="bg-neutral-50 rounded-xl shadow-lg border h-[600px] w-full flex flex-col justify-between">
+        <section className="max-w-2xl w-2/3 h-screen py-24">
+            <div className="bg-neutral-50 p-8 rounded-xl shadow-lg border h-full flex flex-col justify-between">
                 <div>
-                    <div className="flex items-center ml-8">
-                        <div className="flex items-center">
-                            <FaCartShopping className="text-3xl text-sky-500 mr-2" />
-                            <h1 className="text-3xl font-bold whitespace-nowrap text-sky-500">
-                                Shopping List
-                            </h1>
-                        </div>
-                        <div className="flex items-center w-full m-8">
-                            <FaSearch className="absolute pointer-events-none ml-5 text-neutral-500" />
-                            <input
-                                type="text"
-                                value={search}
-                                onChange={(e) => setSearch(e.target.value)}
-                                placeholder="Search"
-                                className="bg-neutral-50 border-neutral-300 border rounded-lg py-3 px-12 w-full"
-                            />
-                        </div>
+                    <div className="flex items-center w-full">
+                        <FaSearch className="absolute pointer-events-none ml-5 text-neutral-500"/>
+                        <input
+                            type="text"
+                            value={search}
+                            onChange={(e) => setSearch(e.target.value)}
+                            placeholder="Search"
+                            className="bg-neutral-50 border-neutral-300 border rounded-lg py-3 px-12 w-full"
+                        />
+
                     </div>
-                    <ul className="h-[370px] overflow-y-scroll">
-                        {searcher(items).map((item, index) => (
-                            <li
-                                key={item.id}
-                                className="flex justify-between items-center first:border-t border-b border-neutral-300 py-4"
-                            >
-                                <div className="flex items-center mx-8">
-                                    <p className="mr-3 text-xs">{index + 1}</p>
+                    <div className="overflow-hidden">
+                        <ul className="">
+                            {searcher(items).map((item, index) => (
+                                <li key={item.id} className="flex">
+                                    <p>{index + 1}</p>
                                     <p
                                         onClick={() => onComplete(item.id)}
-                                        className={`${itemClasses} ${
-                                            item.completed &&
-                                            "line-through text-slate-400"
+                                        className={`${liClasses} ${
+                                            item.completed && "line-through"
                                         }`}
                                     >
                                         {item.name}
                                     </p>
-                                </div>
-                                <button
-                                    className="mx-8 text-lg text-neutral-500"
-                                    onClick={() => onDelete(item.id)}
-                                >
-                                    <BiTrash />
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
+                                    <button onClick={() => onDelete(item.id)}>
+                                        X
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
-                <form
-                    onSubmit={onSubmit}
-                    className="flex items-center w-auto m-8"
-                >
-                    <FaListUl className="absolute pointer-events-none ml-5 text-neutral-500" />
+                <form onSubmit={onSubmit} className="flex items-center w-full">
+                    <FaListUl className="absolute pointer-events-none ml-5 text-neutral-500"/>
                     <input
                         type="text"
                         value={newItem}
                         onChange={(e) => setNewItem(e.target.value)}
                         placeholder="Add new item"
-                        className="bg-neutral-50 border-neutral-300 border rounded-lg py-3 px-12 w-full mr-3"
+                        className="bg-neutral-50 border-neutral-300 border rounded-lg py-3 px-12 w-full mr-2"
                         autoFocus
                     />
-                    <button
-                        className="bg-sky-500 text-neutral-50 rounded-lg py-3 px-5"
-                        type="submit"
-                    >
-                        Add
-                    </button>
+                    <button className="bg-blue-500 text-neutral-50 rounded-lg py-3 px-5" type="submit">Add</button>
                 </form>
             </div>
         </section>
